ab7ed156dc1deb98c744f6c5898fea36ddfd9a4f
ferrarirod <ferrari_rod@hotmail.com>
2019-05-30 16:27:45 -0300

    arquivo implementado

--- core/bancoDeDados/construtorDeQuerys.php
+++ core/bancoDeDados/construtorDeQuerys.php
@@ -8,9 +8,24 @@ class ConstrutorDeQuerys
         $this->pdo = $pdo;
     }
 
-    public function selecionar($tabela,$elemento)
+    public function selecionarTudo($tabela)
     {
-        $coluna = $this->pdo->prepare("select {$elemento} from {$tabela}");
+        $elementos = $this->pdo->prepare("select * from {$tabela}");
+        $elementos->execute();
+        return $elementos->fetchAll(PDO::FETCH_CLASS);
+    }
+    public function inserir($tabela,$paramentros)
+    {
+        $sql = sprintf('insert into %s (%s) values (%s)',$tabela,implode(', ',array_keys($paramentros)),':'.implode(', :',array_keys($paramentros)));
+        try
+        {
+            $elementos = $this->pdo->prepare($sql);
+            $elementos->execute($paramentros);
+        }
+        catch(Exception $e)
+        {
+            $e->getMessage();
+        }    
     }
 }
 ?>
