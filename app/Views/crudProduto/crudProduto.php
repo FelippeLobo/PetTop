bd0e0a813a0c4895a6342d6c705076777959ea5d
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2019-06-29 20:49:01 -0300

    mudando diretorios de css

--- app/Views/crudProduto/crudProduto.php
+++ app/Views/crudProduto/crudProduto.php
@@ -8,9 +8,9 @@
     <title>Produtos</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="Views/crudProduto/assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="app/Views/crudProduto/assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
     <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="Views/crudProduto/assets/css/styles.css">
+    <link rel="stylesheet" href="app/Views/crudProduto/assets/css/styles.css">
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
@@ -139,7 +139,7 @@
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
-    <script src="Views/crudProduto/assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+    <script src="app/Views/crudProduto/assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
