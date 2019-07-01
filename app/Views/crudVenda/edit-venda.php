33cacdf30416da210ec10287606dc3303df1dce1
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2019-06-30 22:04:03 -0300

    css venda redirecionado

--- app/Views/crudVenda/edit-venda.php
+++ app/Views/crudVenda/edit-venda.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="app/Views/crudVenda/css-venda/style-venda.css">
+    <link rel="stylesheet" href="public/cssVenda/style-venda.css">
     <title>Editar venda</title>
   </head>
   <body>
